0f9134660a737395310762cb1c0613c15efa39c9
john <jmulimwau@gmail.com>
2023-11-20 13:01:29 +0300

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState, useEffect } from "react";
+import BotCollection from "./components/BotCollection";
+import BotArmy from "./components/BotArmy";
+import "./App.css";
 
 function App() {
+  const [bots, setBots] = useState([]);
+  const [enlistedBots, setEnlistedBots] = useState([]); 
+
+  useEffect (() =>{
+    //Fetch data from API
+    fetch ('https://my-json-server.typicode.com/John-Mwau/Bot-Battlr/bots')
+    .then ((response) => response.json())
+    .then ((data) => setBots(data))
+    .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  /*useEffect(() => {
+    // Fetch data from the API
+    fetch('https://my-json-server.typicode.com/John-Mwau/Bot-Battlr/bots')
+    .then((resp) => resp.json())
+    .then((data) => setBots(data))
+    .catch((error) => console.error('Error fetching data:', error));
+}, []);
+*/
+const releaseFromArmy = (updatedEnlistedBots) => {
+  // Implement the logic to release bots from the army
+  setEnlistedBots(updatedEnlistedBots);
+};
+
+function handleBotDischarge(bot) {
+  // Remove the bot from enlistedBots in the frontend
+  const updatedEnlistedBots = enlistedBots.filter((enlistedBot) => enlistedBot.id !== bot.id);
+  setEnlistedBots(updatedEnlistedBots);
+}
+ 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <BotArmy 
+        enlistedBots={enlistedBots} 
+        releaseFromYourBotArmy={releaseFromArmy}/> 
+
+      <BotCollection
+       bots={bots} 
+       enlistedBots={enlistedBots} 
+       setEnlistedBots={setEnlistedBots}
+       handleBotDischarge={handleBotDischarge}
+      />
+      
+      
     </div>
   );
 }
-
-export default App;
+export default App
